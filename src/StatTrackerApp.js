d7373da1ad7b29b23c4a361a1a87d3ffeb6b5970
jayson-leach <jleach@ucsd.edu>
2025-06-13 08:50:10 -0700

    Add players text field

--- src/StatTrackerApp.js
+++ src/StatTrackerApp.js
@@ -382,7 +382,7 @@ export default function StatTrackerApp() {
       if (!show) return null;
       return (
       <div style={{ marginTop: 8, color: '#6366f1', fontWeight: 500 }}>
-        Enter the player's name and number, then click Save to add to the roster. Ensure player names are unique.
+        Enter the player's name and number. Ensure player names are unique. You will be able to add more unknown player later.
       </div>
       );
     }
